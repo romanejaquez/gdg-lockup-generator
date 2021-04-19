30ec66973471af23d130b5bbfd759cbbfe6c18b4
Roman Jaquez <roman.jaquez@philips.com>
2021-04-19 12:35:22 -0400

    added navigation and other pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainPageComponent } from './pages/main-page/main-page.component';
+import { OtherPageComponent } from './pages/other-page/other-page.component';
+import { StickerPageComponent } from './pages/sticker-page/sticker-page.component';
 
 const routes: Routes = [
   {
     component: MainPageComponent,
     path: 'main'
+  },
+  {
+    component: StickerPageComponent,
+    path: 'sticker'
+  },
+  {
+    component: OtherPageComponent,
+    path: 'other'
   },
   {
     path: '',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './pages/main-page/main-page.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { OtherPageComponent } from './pages/other-page/other-page.component';
+import { StickerPageComponent } from './pages/sticker-page/sticker-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainPageComponent
+    MainPageComponent,
+    SidebarComponent,
+    OtherPageComponent,
+    StickerPageComponent
   ],
   imports: [
     BrowserModule,
