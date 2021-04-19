206749a32054995e9a22c2b936870c192be3e08a
Roman Jaquez <roman.jaquez@philips.com>
2021-04-19 13:08:45 -0400

    other resources page and config file

--- src/app/pages/other-page/other-page.component.ts
+++ src/app/pages/other-page/other-page.component.ts
@@ -1,13 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import * as imgResources from "../../../assets/resources/imgs/resources-mapping.json";
 
 @Component({
   selector: 'app-other-page',
   templateUrl: './other-page.component.html',
-  styleUrls: ['./other-page.component.scss']
+  styleUrls: ['./other-page.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class OtherPageComponent implements OnInit {
 
   constructor() { }
+
+  imgResourcesList = imgResources.hiResImgs;
 
   ngOnInit(): void {
   }
